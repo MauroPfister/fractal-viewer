43c7cdef65ab28dd57698cb07b6a0ce13565ee23
Mauro Pfister <maurofabio@bluewin.ch>
2019-05-25 14:46:14 +0200

    Cleanup shaderloader and roundbox

--- js/shaderloader.js
+++ js/shaderloader.js
@@ -1,179 +1,179 @@
 
-  // Shader Loader will load any shader you want,
-  // And be able to add in large functions ( such as noise )
-  
-  function ShaderLoader( pathToShaders , pathToChunks ){
+// Shader Loader will load any shader you want,
+// And be able to add in large functions ( such as noise )
 
-    this.shaders       = {};
+function ShaderLoader( pathToShaders , pathToChunks ){
 
-    this.pathToShaders = pathToShaders || "/" ;
-    this.pathToChunks  = pathToChunks || pathToShaders;
+  this.shaders       = {};
 
-    this.shaderChunks  = {};
+  this.pathToShaders = pathToShaders || "/" ;
+  this.pathToChunks  = pathToChunks || pathToShaders;
 
-    this.shadersLoaded = 0;
-    this.shadersToLoad = 0;
+  this.shaderChunks  = {};
 
-  }
+  this.shadersLoaded = 0;
+  this.shadersToLoad = 0;
 
-  /*
+}
 
-    Loads in a shader chunk when told to by
-    onShaderLoaded.
+/*
 
-    The file name of the chunk has to be the same as
-    the reference in the fragment shader where it should be inserted.
+  Loads in a shader chunk when told to by
+  onShaderLoaded.
 
-    For example: 
-    The command $simplexNoise$ in a .vert or .frag file will be replaced
-    with the content of the file simplexNoise.glsl
+  The file name of the chunk has to be the same as
+  the reference in the fragment shader where it should be inserted.
 
-  */
-  ShaderLoader.prototype.loadShaderChunk = function( chunkName ){
+  For example: 
+  The command $simplexNoise$ in a .vert or .frag file will be replaced
+  with the content of the file simplexNoise.glsl
 
-    var path = this.pathToChunks + "/" + chunkName + '.glsl';
+*/
+ShaderLoader.prototype.loadShaderChunk = function( chunkName ){
 
-    var self = this;
-    $.ajax({
-      url:path,
-      dataType:'text',
-      context:{
-        name: chunkName,
-        path: path
-      },
-      complete: function( r ){
-        self.onChunkLoaded( r.responseText , this.name );
-      },
-      error:function( r ){
-        console.log( 'ERROR: Unable to Load Shader' + this.path );
-        self.onChunkLoaded( " NO SHADER LOADED " , this.name );
-      }
-    });
+  var path = this.pathToChunks + "/" + chunkName + '.glsl';
 
-  }
-  
-  ShaderLoader.prototype.onChunkLoaded = function( chunkCode , chunkName ){
-
-    this.shaderChunks[chunkName] = chunkCode;
-    
-  }
-  
-  /*
-  
-     This function loads shader called "shaderName"
-
-  */
-  ShaderLoader.prototype.load = function( shaderName ){
- 
-    var self = this;
-    var type = shaderName.split(".")[1]
-
-    this._beginLoad( shaderName , type);
-
-    // request the file over AJAX
-    $.ajax({
-      url: self.pathToShaders +"/" + shaderName,
-      dataType: 'text',
-      complete: function(r){
-        self.onShaderLoaded( r.responseText , type  );
-      }
-    });
-
-  }
-
-  /*
-   
-     Once a shader is loaded, check to see if there are any extra chunks 
-     we need to find and pull in. 
-
-     Will recall itself, until the chunk has been loaded in.
-
-  */
-  ShaderLoader.prototype.onShaderLoaded = function( shaderCode , type){
-
-    var finalShader = shaderCode;
-    
-    var readyToLoad = true;
-
-
-    var array = finalShader.split( "$" );
-
-    for( var i = 1; i < array.length; i += 2 ){
-
-      var chunkName = array[i].split("$\n")[0];
-
-      if( this.shaderChunks[chunkName] ){
-
-        var tmpShader = finalShader.split( "$" + chunkName + "$" );
-
-        finalShader = tmpShader.join( this.shaderChunks[chunkName] );
-
-      }else{
-
-        readyToLoad = false;
-        this.loadShaderChunk( chunkName );
-
-      }
-
+  var self = this;
+  $.ajax({
+    url:path,
+    dataType:'text',
+    context:{
+      name: chunkName,
+      path: path
+    },
+    complete: function( r ){
+      self.onChunkLoaded( r.responseText , this.name );
+    },
+    error:function( r ){
+      console.log( 'ERROR: Unable to Load Shader' + this.path );
+      self.onChunkLoaded( " NO SHADER LOADED " , this.name );
     }
+  });
 
-    if( readyToLoad ){    
-      
-     if (type == 'vert') {
-       this.shaders[type] = finalShader;
-     } else if (type == 'frag') {
-       this.shaders[type] = finalShader;
-     }
+}
 
-      this._endLoad( finalShader , type );
+ShaderLoader.prototype.onChunkLoaded = function( chunkCode , chunkName ){
+
+  this.shaderChunks[chunkName] = chunkCode;
+  
+}
+
+/*
+
+    This function loads shader called "shaderName"
+
+*/
+ShaderLoader.prototype.load = function( shaderName ){
+
+  var self = this;
+  var type = shaderName.split(".")[1]
+
+  this._beginLoad( shaderName , type);
+
+  // request the file over AJAX
+  $.ajax({
+    url: self.pathToShaders +"/" + shaderName,
+    dataType: 'text',
+    complete: function(r){
+      self.onShaderLoaded( r.responseText , type  );
+    }
+  });
+
+}
+
+/*
+  
+    Once a shader is loaded, check to see if there are any extra chunks 
+    we need to find and pull in. 
+
+    Will recall itself, until the chunk has been loaded in.
+
+*/
+ShaderLoader.prototype.onShaderLoaded = function( shaderCode , type){
+
+  var finalShader = shaderCode;
+  
+  var readyToLoad = true;
+
+
+  var array = finalShader.split( "$" );
+
+  for( var i = 1; i < array.length; i += 2 ){
+
+    var chunkName = array[i].split("$\n")[0];
+
+    if( this.shaderChunks[chunkName] ){
+
+      var tmpShader = finalShader.split( "$" + chunkName + "$" );
+
+      finalShader = tmpShader.join( this.shaderChunks[chunkName] );
 
     }else{
 
-      var self = this;
-      setTimeout( function(){
-        self.onShaderLoaded( finalShader , type  )
-      }, 300 );
+      readyToLoad = false;
+      this.loadShaderChunk( chunkName );
 
     }
 
   }
 
-  
-  // might add something later...
-  ShaderLoader.prototype._beginLoad = function( shaderName , type  ){
-    this.shadersToLoad ++;
-    this.beginLoad( shaderName , type  );
-  }
-  
-  ShaderLoader.prototype._endLoad = function( shaderText, type ){
-    this.shadersLoaded ++;
-
-    if( this.shadersLoaded == this.shadersToLoad ){
-      this.shaderSetLoaded();
-    }
+  if( readyToLoad ){    
     
-    this.endLoad( shaderText, type  );
+    if (type == 'vert') {
+      this.shaders[type] = finalShader;
+    } else if (type == 'frag') {
+      this.shaders[type] = finalShader;
+    }
+
+    this._endLoad( finalShader , type );
+
+  }else{
+
+    var self = this;
+    setTimeout( function(){
+      self.onShaderLoaded( finalShader , type  )
+    }, 300 );
 
   }
-  
+
+}
 
 
-  ShaderLoader.prototype.setValue = function( shader , name , value ){
+// might add something later...
+ShaderLoader.prototype._beginLoad = function( shaderName , type  ){
+  this.shadersToLoad ++;
+  this.beginLoad( shaderName , type  );
+}
 
-    //console.log( name , value );
+ShaderLoader.prototype._endLoad = function( shaderText, type ){
+  this.shadersLoaded ++;
 
-    var a = '@'+name;
-    //console.log( a );
-   
-    var replaced = false;
-
-    var newStr = shader.replace( a , function(token){replaced = true; return value;}); 
-
-    console.log( 'replaced' , replaced );
-    return newStr;
-  
+  if( this.shadersLoaded == this.shadersToLoad ){
+    this.shaderSetLoaded();
   }
+  
+  this.endLoad( shaderText, type  );
 
-  ShaderLoader.prototype.shaderSetLoaded = function(){}
-  ShaderLoader.prototype.endLoad = function(){}
-  ShaderLoader.prototype.beginLoad = function(){}
+}
+
+
+
+ShaderLoader.prototype.setValue = function( shader , name , value ){
+
+  //console.log( name , value );
+
+  var a = '@'+name;
+  //console.log( a );
+  
+  var replaced = false;
+
+  var newStr = shader.replace( a , function(token){replaced = true; return value;}); 
+
+  console.log( 'replaced' , replaced );
+  return newStr;
+
+}
+
+ShaderLoader.prototype.shaderSetLoaded = function(){}
+ShaderLoader.prototype.endLoad = function(){}
+ShaderLoader.prototype.beginLoad = function(){}
